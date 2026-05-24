78d6971e20ce58fdda531505e6587ea6ccf34895
kamaeepk <bc240408434mwa@vu.edu.pk>
2026-05-24 20:30:20 +0500

    Minor text changes

--- components/karigar/CalculateWagesSheet.tsx
+++ components/karigar/CalculateWagesSheet.tsx
@@ -404,7 +404,7 @@ export function CalculateWagesSheet({
                           onClick={() => fillAdvanceBalance(adv)}
                           className="mt-6 shrink-0 text-xs font-semibold text-brand"
                         >
-                          Poora Adv Katoti
+                          Sara katay
                         </button>
                       </div>
                     </div>
